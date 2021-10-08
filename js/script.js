68653ee2d18a046931553136dc239c67fe274fee
EddieFreedome <eddie.freed@tutanota.com>
2021-10-08 15:55:02 +0200

    Completato esercizio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-const prezzoBiglietto = "0,21";
-prezzoBiglietto = parseFloat(prezzoBiglietto);
+const prezzoChilometro = 0.21;
+let etaUtente = prompt("Inserisci la tua eta'");
+etaUtente = parseInt(etaUtente)
+const distanza = parseInt(prompt("Inserisci il numero di chilometri che vuoi percorrere"));
 
 //prezzo
-prezzoGiovani = prezzobiglietto
+const prezzoStandard = prezzoChilometro * distanza.toFixed(2);
+let prezzoJunior = prezzoStandard - (prezzoStandard / 100 * 20).toFixed(2);
+let prezzoSenior = prezzoStandard - (prezzoStandard / 100 * 40).toFixed(2);
+
 //eta utente
-let etaUtente = prompt("Inserisci la tua eta'");
-const distanza = prompt("Inserisci il numero di chilometri che vuoi percorrere");
 
 if (etaUtente < 18) {
-    alert(`Promo Junior: ti e' stato applicato uno sconto di ${totaleGiovani}`)
+    alert(`Promo Junior: ti e' stato applicato uno sconto del 20%. Il prezzo del tuo biglietto è: ${prezzoJunior}`)
 }
-else{
 
+if (etaUtente >= 18 && etaUtente <= 65) {
+    alert(`Promo Standard: Il prezzo del tuo biglietto è: ${prezzoStandard}`)
+}
+
+if (etaUtente > 65) {
+    alert(`Promo Senior: ti e' stato applicato uno sconto del 40%. Il prezzo del tuo biglietto è: ${prezzoSenior}`)
 }
 
 

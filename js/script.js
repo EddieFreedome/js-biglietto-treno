e32d3a124da47cdc96672a63b9e272c589a28027
EddieFreedome <eddie.freed@tutanota.com>
2021-10-08 15:09:28 +0200

    added js variables

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const prezzoBiglietto = "0,21";
+prezzoBiglietto = parseFloat(prezzoBiglietto);
+
+//prezzo
+prezzoGiovani = prezzobiglietto
+//eta utente
+let etaUtente = prompt("Inserisci la tua eta'");
+const distanza = prompt("Inserisci il numero di chilometri che vuoi percorrere");
+
+if (etaUtente < 18) {
+    alert(`Promo Junior: ti e' stato applicato uno sconto di ${totaleGiovani}`)
+}
+else{
+
+}
+
+
